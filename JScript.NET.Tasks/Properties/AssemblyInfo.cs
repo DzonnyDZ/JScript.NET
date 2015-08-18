1f30fd585550e188dda8562ee33a9457ae8cf0db
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-08-18 14:30:15 +0200

    Starting Over

--- JScript.NET.Tasks/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("JScript.NET.Tasks")]
-[assembly: AssemblyDescription("Contains MSBuild tasks for JScriupt.NET compiler")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("3dc58ab3-f67c-4481-a981-5e8a194118ae")]
---
+++ JScript.NET/JScript.NET.ProjectType/VsPackage.cs
@@ -0,0 +1,54 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+namespace Dzonny.JScriptNet
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// This package is required if you want to define adds custom commands (ctmenu)
+    /// or localized resources for the strings that appear in the New Project and Open Project dialogs.
+    /// Creating project extensions or project types does not actually require a VSPackage.
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Description("A custom project type based on CPS")]
+    [Guid(VsPackage.PackageGuid)]
+    public sealed class VsPackage : Package
+    {
+        /// <summary>
+        /// The GUID for this package.
+        /// </summary>
+        public const string PackageGuid = "b584f11e-5e77-40e8-bbfd-f70b550504ba";
+
+        /// <summary>
+        /// The GUID for this project type.  It is unique with the project file extension and
+        /// appears under the VS registry hive's Projects key.
+        /// </summary>
+        public const string ProjectTypeGuid = "e452ebf3-3bbb-4a96-b835-ae6ecaeab859";
+
+        /// <summary>
+        /// The file extension of this project type.  No preceding period.
+        /// </summary>
+        public const string ProjectExtension = "jsnproj";
+
+        /// <summary>
+        /// The default namespace this project compiles with, so that manifest
+        /// resource names can be calculated for embedded resources.
+        /// </summary>
+        internal const string DefaultNamespace = "Dzonny.JScriptNet";
+    }
+}
