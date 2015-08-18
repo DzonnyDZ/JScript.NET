03f18c9291aa28e818c55cbc9e7dcf8f95d76ef3
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-08-18 14:40:26 +0200

    move stuff around

---
+++ JScript.NET.ProjectType/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: Microsoft.VisualStudio.Shell.ProvideCodeBase(CodeBase = @"$PackageFolder$\Dzonny.JScriptNet.dll")]
+
+[assembly: AssemblyTitle("JScript.NET.ProjectType")]
+[assembly: AssemblyDescription("JScript.NET project type for Visual Studio")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e82e795-156f-4b68-885d-83f2a507d9a7")]
