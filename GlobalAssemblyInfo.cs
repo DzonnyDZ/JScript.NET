8fa8ee93409d6df43d1a6b8027da65ad622fe181
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-08-18 14:32:02 +0200

    Put some stuff back

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Đonny")]
+[assembly: AssemblyProduct("JScript.NET VS")]
+[assembly: AssemblyCopyright("Copyright © Đonny 2015")]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
