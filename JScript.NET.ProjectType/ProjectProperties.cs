642167e8ac70893ea61258bc0e47fb9bd3d37cb0
Jan Záruba <jan.zaruba7393@embedit.cz>
2015-09-15 17:08:28 +0200

    Add VSLangProj.Shared to solution + NuGetUpdate

--- JScript.NET.ProjectType/ProjectProperties.cs
+++ JScript.NET.ProjectType/ProjectProperties.cs
@@ -11,7 +11,6 @@ namespace Dzonny.JScriptNet
     using Microsoft.VisualStudio.ProjectSystem.Utilities;
 
     [Export]
-    [AppliesTo(MyUnconfiguredProject.UniqueCapability)]
     internal partial class ProjectProperties : StronglyTypedPropertyAccess
     {
         /// <summary>
